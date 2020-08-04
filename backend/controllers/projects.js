b37e4e02e6d67eb7c94c5a48a7e4115ff22dd39a
k-fowler <fowler.krystian@gmail.com>
2020-08-04 13:50:46 +0200

    Create async/await and error handling middleware

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -1,113 +1,84 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Project = require('../models/Project');
 
 // @desc Get all projects
 // @route GET /api/v1/projects
 // @access Private
-exports.getProjects = async (req, res, next) => {
-  try {
-    const projects = await Project.find();
-    res.status(200).json({
-      success: true,
-      count: projects.length,
-      data: projects,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    });
-  }
-};
+exports.getProjects = asyncHandler(async (req, res, next) => {
+  const projects = await Project.find();
+  res.status(200).json({
+    success: true,
+    count: projects.length,
+    data: projects,
+  });
+});
 
 // @desc Get a single project
 // @route GET /api/v1/projects/:id
 // @access Private
-exports.getProject = async (req, res, next) => {
-  try {
-    const project = await Project.findById(req.params.id);
+exports.getProject = asyncHandler(async (req, res, next) => {
+  const project = await Project.findById(req.params.id);
 
-    if (!project) {
-      return res.status(400).json({
-        success: false,
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: project,
-    });
-  } catch (error) {
-    // res.status(400).json({
-    //   success: false,
-    // });
-    next(error);
+  if (!project) {
+    return next(
+      new ErrorResponse(`Project not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: project,
+  });
+});
 
 // @desc Create new project
 // @route POST /api/v1/projects
 // @access Private
-exports.createProject = async (req, res, next) => {
-  try {
-    const project = await Project.create(req.body);
+exports.createProject = asyncHandler(async (req, res, next) => {
+  const project = await Project.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: project,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: project,
+  });
+});
 
 // @desc Update project
 // @route PUT /api/v1/projects/:id
 // @access Private
-exports.updateProject = async (req, res, next) => {
-  try {
-    const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateProject = asyncHandler(async (req, res, next) => {
+  const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!project) {
-      return res.status(400).json({
-        success: false,
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: project,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    });
+  if (!project) {
+    return next(
+      new ErrorResponse(`Project not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: project,
+  });
+});
 
 // @desc Delete project
 // @route DELETE /api/v1/projects/:id
 // @access Private
-exports.deleteProject = async (req, res, next) => {
-  try {
-    const project = await Project.findByIdAndDelete(req.params.id);
+exports.deleteProject = asyncHandler(async (req, res, next) => {
+  const project = await Project.findByIdAndDelete(req.params.id);
 
-    if (!project) {
-      return res.status(400).json({
-        success: false,
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-    });
+  if (!project) {
+    return next(
+      new ErrorResponse(`Project not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
@@ -25,6 +26,8 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount routers
 app.use('/api/v1/projects', projects);
+
+app.use(errorHandler);
 
 app.use(cors());
 
