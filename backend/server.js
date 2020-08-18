e64c30f575ff323809a18e108f4e541c2d8e5be9
k-fowler <fowler.krystian@gmail.com>
2020-08-18 19:31:50 +0200

    Implement rate limiting, prevent http param pollution and enable CORS

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
-const cors = require('cors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -49,6 +51,20 @@ app.use(helmet());
 // Prevent XSS attacks
 app.use(xss());
 
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -60,8 +76,6 @@ app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
 
 app.use(errorHandler);
-
-app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
