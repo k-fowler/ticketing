4416a209b9cc2b9068e999c8bf9c9febb01942c2
k-fowler <fowler.krystian@gmail.com>
2020-08-18 19:23:13 +0200

    Implement XSS protection and HTTP security headers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -40,6 +42,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
